1fa7727b986d8b4595836931ea90c5a0ba829c63
Mauricio Robayo <rfmajo@gmail.com>
2019-08-23 16:06:28 -0500

    Fix testing cases for return values

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,9 +1,9 @@
 require_relative "../enumerable_methods.rb"
 
-
-
 RSpec.describe Enumerable do
-  let(:test_array) {[1,2,3,2]}
+  
+  let(:test_array) { [1, 2, 3, 2] }
+
   describe "#my_each" do
     it "returns the same initial array" do
       expect(test_array.my_each { |i| i }).to eql(test_array)
@@ -14,14 +14,8 @@ RSpec.describe Enumerable do
     end
   end
 
-  describe "#my_each_with_index" do
-    it "return the same array with the corresponding indexes" do
-      expect(test_array.my_each_with_index {|index, item| index, item}).to eql([0,1,2,3],)
-    end  
-  end
-
   describe "#my_select" do
-    it "return the same array with the corresponding indexes" do
+    it "return an array with just the items greater than 2" do
       expect(test_array.my_select {|item| item > 2}).to eql([3])
     end  
   end
@@ -33,32 +27,32 @@ RSpec.describe Enumerable do
   end
 
   describe "#my_any?" do
-    it "return true if one number match the condition" do
-      expect(test_array.my_any? { |i| i >= 0 }).to eql(false)
+    it "return true if at least one number match the condition" do
+      expect(test_array.my_any? { |i| i >= 0 }).to eql(true)
     end  
   end
 
   describe "#my_none?" do
-    it "return the same array with the corresponding indexes" do
+    it "return true if no item is equal to five" do
       expect(test_array.my_none? { |i| i == 5 }).to eql(true)
     end  
   end
 
   describe "#my_count" do
-    it "return the same array with the corresponding indexes" do
+    it "return the number of times the element 2 is in the array" do
       expect(test_array.my_count { |i| i == 2 }).to eql(2)
     end  
   end
 
   describe "#my_map" do
-    it "return the same array with the corresponding indexes" do
-      expect(test_array.my_map { |i| i * 5 }).to eql([5,10,15,20],)
+    it "return a new array with the items multiplied by 5" do
+      expect(test_array.my_map { |i| i * 5 }).to eql([5, 10, 15, 10])
     end  
   end
 
   describe "#my_inject" do
-    it "return the same array with the corresponding indexes" do
-      expect(test_array.my_inject { |accumulator, i| accumulator + i }).to eql(10)
+    it "return the sum of the elements of the array" do
+      expect(test_array.my_inject { |accumulator, i| accumulator + i }).to eql(8)
     end  
   end
 
