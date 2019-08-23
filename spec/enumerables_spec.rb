f0097b26834105d7c9c4d86baeee0222433d91ef
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2019-08-23 15:56:12 -0500

    add testing cases

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,9 +1,11 @@
 require_relative "../enumerable_methods.rb"
 
+
+
 RSpec.describe Enumerable do
+  let(:test_array) {[1,2,3,2]}
   describe "#my_each" do
     it "returns the same initial array" do
-      test_array = [1, 2, 3]
       expect(test_array.my_each { |i| i }).to eql(test_array)
     end
     it "returns the same empty array" do
@@ -11,4 +13,11 @@ RSpec.describe Enumerable do
       expect(test_array.my_each { |i| i }).to eql(test_array)
     end
   end
+
+  describe "#my_each_with_index" do
+    it "return the same array with the corresponding indexes" do
+      expect(test_array.my_each_with_index {|index, item| index, item}).to eql(test_array,[0,1,2,3])
+    end  
+
+
 end
